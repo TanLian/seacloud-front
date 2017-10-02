afaaeecacfa7b176204dbc4e08193a5f0e92fcf3
TanLian <1799938739@qq.com>
2017-10-02 10:54:22 +0800

    优化文件列表页面的显示

--- src/components/LocalFiles.vue
+++ src/components/LocalFiles.vue
@@ -4,14 +4,21 @@
     <div class="right-panel">
       <div class="right-top">
         <div class="path-block">
-          <a href="/">
-            <i class="fa fa-home fa-fw"></i>
-          </a>
+          <el-breadcrumb separator="/">
+            <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          </el-breadcrumb>
         </div>
         <div class="ops-block">
-            <el-button type="primary">上传文件</el-button>
-            <el-button>新建文件</el-button>
-            <el-button>新建目录</el-button>
+            <el-button type="primary">上传<i class="el-icon-upload el-icon--right"></i></el-button>
+            <el-dropdown>
+              <el-button type="text">
+                新建<i class="el-icon-caret-bottom el-icon--right"></i>
+              </el-button>
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item><label>新建文件</label></el-dropdown-item>
+                <el-dropdown-item><label>新建目录</label></el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
           </div>
       </div>
       <div class="right-main">
@@ -19,7 +26,6 @@
           ref="multipleTable"
           :data="filelist"
           stripe
-          border
           tooltip-effect="dark"
           style="width: 100%">
           <el-table-column
@@ -29,17 +35,27 @@
           <el-table-column
             prop="name"
             label="名称"
-            width="120">
+            sortable
+            width="360">
           </el-table-column>
           <el-table-column
             prop="size"
             label="大小"
+            sortable
             width="120">
           </el-table-column>
           <el-table-column
             prop="mtime"
             label="修改日期"
+            sortable
+            width="240"
             show-overflow-tooltip>
+          </el-table-column>
+          <el-table-column label="操作" width="150">
+            <template scope="scope">
+              <i class="el-icon-star-off"></i>
+              <el-button size="small" @click="handleShare(scope.$index, scope.row)">分享</el-button>
+            </template>
           </el-table-column>
         </el-table>
       </div>
@@ -56,6 +72,12 @@
         filelist: []
       }
     },
+    methods: {
+      handleShare(index, row) {
+        console.log(index)
+        console.log(row)
+      }
+    },
     mounted() {
       let dir_path = this.$route.query.p ? this.$route.query.p : '/'
 
@@ -67,6 +89,7 @@
           obj['name'] = files[index]['Name']
           obj['size'] = files[index]['Size']
           obj['mtime'] = files[index]['MtimeRelative']
+          obj['type'] = files[index]['Type']
           this.filelist.push(obj)
         }
       }, (response) => {
@@ -90,12 +113,14 @@
       width: 100%;
       height: 60px;
       //background-color: #ccc;
+      overflow: hidden;
       position: relative;
       .path-block {
         height: 100%;
         line-height: 60px;
         margin-right: 10px;
         float: left;
+        padding-top: 25px;
         a {
           cursor: pointer;
           font-weight: bold;
@@ -112,7 +137,7 @@
     .right-main {
       background-color: #ccc;
       position: relative;
-      top: 60px;
+      top: 20px;
       width: 100%;
     }
   }
