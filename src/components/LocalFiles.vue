2b7ae5e20c870ead4c4d01b1d5df47b43ddcf5a2
TanLian <1799938739@qq.com>
2017-10-14 11:05:54 +0800

    完成了删除文件的功能，重命名、移动、复制能够取到相关数据

--- src/components/LocalFiles.vue
+++ src/components/LocalFiles.vue
@@ -9,14 +9,15 @@
           </el-breadcrumb>
         </div>
         <div class="ops-block">
-            <el-upload
-                  :action="uploadAction"
-                  :on-success="handleSuccess"
-                  :before-upload="handleBefore"
-                  :file-list="filestoupload"
-                  style="float:left;">
-              <el-button type="primary">上传<i class="el-icon-upload el-icon--right"></i></el-button>
-          </el-upload>
+            <el-button type="primary">
+              <input type="file" name="file" multiple @change="changeFile($event)">
+              上传<i class="el-icon-upload el-icon--right"></i>
+            </el-button>
+            <!--<div class="upload-file-btn">
+              <span>上传</span>
+              <i class="el-icon-upload el-icon--right"></i>
+              <input type="file" name="file" multiple @change="changeFile($event)">
+            </div>-->
             <el-dropdown class="new-btn">
               <el-button type="text">
                 新建<i class="el-icon-caret-bottom el-icon--right"></i>
@@ -68,8 +69,29 @@
               <a href="#" @click.prevent="handleDownload(scope.$index, scope.row)">
                 <i class="fa fa-download"></i>
               </a>
+              <a href="#" @click.prevent="handleDelete(scope.$index, scope.row)">
+                <i class="fa fa-trash-o fa-lg"></i>
+              </a>
               <i class="el-icon-star-off"></i>
-              <el-button size="small" @click="handleShare(scope.$index, scope.row)">分享</el-button>
+              <el-dropdown trigger="click" @command="handleCommand">
+                <span class="el-dropdown-link">
+                  <i class="el-icon-caret-bottom el-icon--right"></i>
+                </span>
+                <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item command="rename" :row="scope.row" ref="rename">
+                      <i class="fa fa-pencil"></i>
+                      <span style="margin-left:8px;">重命名</span>
+                    </el-dropdown-item>
+                  <el-dropdown-item command="move">
+                    <i class="fa fa-cut"></i>
+                    <span style="margin-left:8px;">移动</span>
+                  </el-dropdown-item>
+                  <el-dropdown-item command="copy">
+                    <i class="fa fa-copy"></i>
+                    <span style="margin-left:8px;">复制</span>
+                  </el-dropdown-item>
+                </el-dropdown-menu>
+              </el-dropdown>
             </template>
           </el-table-column>
         </el-table>
@@ -87,35 +109,77 @@
       return {
         currentPath: "/",
         filelist: [],
-        filestoupload: [],
-        uploadAction: "api/api/upload/files"
+        uploadAction: ""
       }
     },
     mixins: [Common],
     methods: {
+      handleCommand(command, a) {
+        console.log(command)
+        let data = this.$refs.rename.$attrs.row
+        switch (command) {
+          case 'rename':
+              console.log(data)
+            break
+          case 'move':
+              console.log(data)
+            break
+          case 'copy':
+              console.log(data)
+            break
+          default:
+            break
+        }
+      },
+      handleRename(index, row) {
+        console.log(index)
+        console.log(row)
+      },
       handleShare(index, row) {
         console.log(index)
         console.log(row)
       },
-      handleSuccess(response){
+      changeFile(event) {
+        var file = event.target.files[0]
+        console.log(file)
+
+        //获取上传链接
+        this.$http.get("api/api/file/get_tmp_upload_link", {params:{"path":this.currentPath}}).then((response) => {
           console.log(response)
-          let files = response.files
-          console.log(files)
-          for (let index = 0; index < files.length; index++) {
-            console.log(files[index])
-            let obj = {}
-            obj['name'] = files[index]['Name']
-            obj['size'] = this.formatSize(files[index]['Size'])
-            obj['mtime'] = files[index]['MtimeRelative']
-            obj['type'] = files[index]['Type']
-            this.filelist.push(obj)
-          }
-          //this.filestoupload = [{name: response.name, url: imgBaseUrl + response.photo}]
-      },
-      handleBefore(){
-        return this.filestoupload.length === 1 ? false : true // 只让它上传一张
+          let data = response.data
+          let link = data['link']
+          console.log(link)
+          link = 'api' + link
+
+          let formData = new FormData()
+          formData.append('file', file)
+          this.$http.post(link, formData, {
+            headers: {
+              "Content-Type": "multipart/form-data"
+            }
+          }).then((response) => {
+            console.log(response)
+            let data = response.data
+            let files = data.files
+            for (let index = 0; index < files.length; index++) {
+              let obj = {}
+              obj['name'] = files[index]['Name']
+              obj['size'] = this.formatSize(files[index]['Size'])
+              obj['mtime'] = files[index]['MtimeRelative']
+              obj['type'] = files[index]['Type']
+              this.filelist.push(obj)
+            }
+          }, (response) => {
+            console.error(response)
+          })
+
+        }, (response) => {
+          console.error(response)
+        })
       },
       handleDownload(index, row) {
+        console.log("下载文件")
+        console.log(row)
         this.$http.get('api/api/file/get_tmp_download_link', {params:{"path":this.currentPath + row.name}}).then((response) => {
           console.log(response)
           let data = response.data
@@ -126,6 +190,41 @@
         }, (response) => {
           console.error(response)
         })
+      },
+      deleteConfirm(index, row) {
+        let filename = row.name
+        let tp = row.type
+        let content = ""
+        if (tp === "dir") {
+          content = "此操作将删除目录" + filename + ",是否继续？"
+        }else {
+          content = "此操作将删除文件" + filename + ",是否继续？"
+        }
+        this.$confirm(content, '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.$http.get('api/api/file/delete', {params:{"p":this.currentPath + filename}}).then((response) => {
+            console.log(response)
+            this.filelist = this.filelist.filter(function(i){ return i.name != filename })
+            this.$message({
+              type: 'success',
+              message: '删除成功!'
+            });
+          }, (response) => {
+            console.error(response)
+          })
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          });          
+        });
+      },
+      handleDelete(index, row) {
+        console.log(row)
+        this.deleteConfirm(index, row)
       }
     },
     mounted() {
